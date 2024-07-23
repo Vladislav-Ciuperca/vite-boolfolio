1cd76b5ac15c9688cb0ae8406950db77f3df93e9
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-07-23 02:01:29 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script>
+  import Componente from './components/Componente.vue'
+
+
+
+  export default {
+    components: {
+      Componente
+    },
+    data() {
+      return {
+        appTitle: "Vue-Boolfolio",
+      }
+    },
+    methods: {
+      // placeholder
+    },
+    created() {
+      // placeholdser
+    },
+    mounted() {
+      // placeholder
+    }
+  }
+</script>
+
+<!-- //////////////////////////////// -->
+
+<template>
+  <h2>cioa Vue mondo</h2>
+  <Componente />
+</template>
+
+<!-- //////////////////////////////// -->
+
+<style scoped>
+  h2 {
+    padding: 1rem;
+  }
+</style>
+
+<!-- //////////////////////////////// -->
+
+<script>
+
+
+
+</script>
